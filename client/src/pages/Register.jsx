7262405a42dd6e1f43e2a25c5b65455b0bb379b7
mohamedAliBn <mohamed3alibnjhhh@gmail.com>
2024-04-02 02:23:51 +0000

    setup register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import { Logo } from '../components'
 import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+import FormRow from '../components/FormRow'
 
 function Register() {
   return (
-    <div>
-        <h1>register page</h1>
-        <Link to={"/login"}>go to login page</Link>
-    </div>
+    <Wrapper>
+      <form className='form'>
+        <Logo/>
+        <h4>Register</h4>
+        <FormRow type="text" name="name" defaultValue="ali"/>
+        <FormRow type="text" labelText="Last name" name="lastName " defaultValue="ali"/>
+        <FormRow type="text" name="location" defaultValue="location"/>
+        <FormRow type="email" name="email" defaultValue="ali123@gmail.com"/>
+        <FormRow type="password" name="password" defaultValue="ali200110"/>
+        
+        <button type='submit' className='btn btn-block'>submit</button>
+        <p>
+            Already a member?
+            <Link to={"/login"} className='member-btn' >Login</Link> 
+        </p>
+      </form>
+    </Wrapper>
   )
 }
 
